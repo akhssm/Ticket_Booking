854a7ce469f4ce186ec9e0127ee98a74725207a6
Akshay <mandaakshay20@gmail.com>
2026-01-04 17:12:52 +0530

    Created movie trailer section

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../Components/HeroSection.jsx'
 import FeaturedSection from '../Components/FeaturedSection.jsx'
+import TrailersSection from '../Components/TrailersSection.jsx'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <FeaturedSection />
+      <TrailersSection />
     </>
   )
 }
