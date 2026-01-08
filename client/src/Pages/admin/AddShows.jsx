b8bbbac74b5a61b3fcd6a7a1d2d1921d7090bf0f
Akshay <mandaakshay20@gmail.com>
2026-01-08 16:14:44 +0530

    Completed frontend part successfully

--- client/src/Pages/admin/AddShows.jsx
+++ client/src/Pages/admin/AddShows.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets';
 import Loading from '../../Components/Loading';
 import Title from '../../Components/admin/Title';
-import { CheckIcon, StarIcon } from 'lucide-react';
+import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react';
 import { kConverter } from '../../Lib/kConverter';
 
 const AddShows = () => {
@@ -115,6 +115,35 @@ const AddShows = () => {
         </div>
       </div>
 
+      {/* Display Selected Times */}
+      {Object.keys(dateTimeSelection).length > 0 && (
+        <div className="mt-6">
+          <h2 className="mb-2">Selected Date-Time</h2>
+          <ul className="space-y-3">
+            {Object.entries(dateTimeSelection).map(([date, times]) => (
+              <li key={date}>
+                <div className="font-medium">{date}</div>
+                <div className="flex flex-wrap gap-2 mt-1 text-sm">
+                  {times.map((time) => (
+                    <div keys={time} className="border border-primary
+                    px-2 py-1 flex items-center rounded">
+                      <span>{time}</span>
+                      <DeleteIcon onClick={() => handleRemoveTime
+                        (date, time)} width={15} className="ml-2
+                        text-red-500 hover:text-red-700
+                        cursor-pointer" />
+                    </div>
+                  ))}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button className="bg-primary text-white px-8 py-2 mt-2 rounded
+      hover:bg-primary/90 transition-all cursor-pointer">
+             Add Show
+      </button>
     </>
   ) : <Loading />
 }
