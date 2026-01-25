697d834e43f01809f0d245942d1e6de00f48efb4
Akshay <mandaakshay20@gmail.com>
2026-01-25 12:25:08 +0530

    Fetching data from backend and displaying in dashboard page

--- client/src/Pages/admin/Dashboard.jsx
+++ client/src/Pages/admin/Dashboard.jsx
@@ -5,8 +5,13 @@ import Loading from '../../Components/Loading'
 import Title from '../../Components/admin/Title'
 import BlurCircle from '../../Components/BlurCircle'
 import { dateFormat } from '../../Lib/dateFormat'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Dashboard = () => {
+
+  const {axios, getToken, user, image_base_url} = useAppContext()
+
   const currency = import.meta.env.VITE_CURRENCY
 
   const [dashboardData, setDashboardData] = useState({
@@ -19,16 +24,48 @@ const Dashboard = () => {
   const [loading, setLoading] = useState(true)
 
   const dashboardCards = [
-    { title: 'Total Bookings', value: dashboardData.totalBookings, icon: ChartLineIcon },
-    { title: 'Total Revenue', value: currency + dashboardData.totalRevenue, icon: CircleDollarSignIcon },
-    { title: 'Active Shows', value: dashboardData.activeShows.length, icon: PlayCircleIcon },
-    { title: 'Total Users', value: dashboardData.totalUser, icon: UsersIcon }
+    { title: 'Total Bookings', value: dashboardData?.totalBookings ?? 0, icon: ChartLineIcon },
+    { title: 'Total Revenue', value: currency + (dashboardData?.totalRevenue ?? 0), icon: CircleDollarSignIcon },
+    { title: 'Active Shows', value: dashboardData?.activeShows?.length ?? 0, icon: PlayCircleIcon },
+    { title: 'Total Users', value: dashboardData?.totalUser ?? 0, icon: UsersIcon }
   ]
 
+  const fetchDashboardData = async () => {
+    try {
+      setLoading(true)
+
+      const token = await getToken()
+
+      const { data } = await axios.get("/api/admin/dashboard", {
+      headers: { Authorization: `Bearer ${token}` }})
+
+      console.log("DASHBOARD API FULL RESPONSE:", data)
+      const dashboard = data?.getDashboardData
+
+      if (!dashboard) {
+        toast.error("Invalid dashboard data")
+        return
+      }
+
+      setDashboardData({
+        totalBookings: dashboard.totalBookings || 0,
+        totalRevenue: dashboard.totalRevenue || 0,
+        totalUser: dashboard.totalUser || 0,
+        activeShows: dashboard.activeShows || []
+      })
+    } catch (error) {
+      console.error(error)
+      toast.error(error.response?.data?.message || "Error fetching dashboard data")
+    } finally {
+      setLoading(false)
+    }
+  }
+
   useEffect(() => {
-    setDashboardData(dummyDashboardData)
-    setLoading(false)
-  }, [])
+      if(user) {
+          fetchDashboardData();
+      }
+  }, [user]);
 
   if (loading) return <Loading />
 
@@ -65,7 +102,7 @@ const Dashboard = () => {
       <div className="relative flex flex-wrap gap-6 mt-4 max-w-5xl">
         <BlurCircle top="100px" left="-10%" />
 
-        {dashboardData.activeShows.map((show) => (
+        {dashboardData?.activeShows?.map((show) => (
           <div
             key={show._id}
             className="w-55 rounded-lg overflow-hidden
@@ -73,30 +110,30 @@ const Dashboard = () => {
             hover:-translate-y-1 transition duration-300"
           >
             <img
-              src={show.movie.poster_path}
-              alt=""
+              src={image_base_url + show.movie.poster_path}
+              alt={show.movie.title}
               className="h-60 w-full object-cover"
             />
-
+              
             <p className="font-medium p-2 truncate">
               {show.movie.title}
             </p>
 
             <div className="flex items-center justify-between px-2">
               <p className="text-lg font-medium">
-                {currency} {show.showPrice}
+                {currency}{show.showPrice}
               </p>
 
-              <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+              <p className="flex items-center gap-1 text-sm text-gray-400">
                 <StarIcon className="w-4 h-4 text-primary fill-primary" />
-                {show.movie.vote_average.toFixed(1)}
+                {show.movie.vote_average?.toFixed(1)}
               </p>
             </div>
 
             <p className="px-2 pt-2 text-sm text-gray-500">
               {dateFormat(show.showDateTime)}
             </p>
-          </div>   
+          </div>
         ))}
       </div>
     </div>
