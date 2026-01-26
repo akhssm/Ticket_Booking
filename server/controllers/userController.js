c5b95cea3b0a5627018e5d5c53d9ddf69a1fc1ea
Akshay <mandaakshay20@gmail.com>
2026-01-26 15:53:22 +0530

    Made changes in user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -22,48 +22,65 @@ export const getUserBookings = async (req, res) => {
 // API Controller Function to Update Favorite Movie in Clerk User Matadata
 export const updateFavourite = async (req, res) => {
   try {
-    const { movieId } = req.body;
-    const userId = req.auth().userId;
+    const { movieId } = req.body
+    const userId = req.auth().userId
+
+    if (!movieId || typeof movieId !== "string") {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid movieId"
+      })
+    }
 
     // Get user from Clerk
-    const user = await clerkClient.users.getUser(userId);
+    const user = await clerkClient.users.getUser(userId)
 
-    // Ensure favourites array exists
-    if (!user.privateMetadata.favourites) {
-      user.privateMetadata.favourites = [];
-    }
+    const favourites = Array.isArray(user.privateMetadata.favourites)
+      ? user.privateMetadata.favourites
+      : []
 
-    // Add or remove movieId from favourites
-    if (!user.privateMetadata.favourites.includes(movieId)) {
-      user.privateMetadata.favourites.push(movieId);
+    let updatedFavourites
+
+    if (favourites.includes(movieId)) {
+      updatedFavourites = favourites.filter(id => id !== movieId)
     } else {
-      user.privateMetadata.favourites = user.privateMetadata.favourites.filter(
-        item => item !== movieId
-      );
+      updatedFavourites = [...favourites, movieId]
     }
 
-    // Update user metadata in Clerk
-    await clerkClient.users.updateUserMetadata(userId, { privateMetadata: user.privateMetadata });
+    // Update Clerk metadata
+    await clerkClient.users.updateUserMetadata(userId, {
+      privateMetadata: {
+        ...user.privateMetadata,
+        favourites: updatedFavourites
+      }
+    })
 
-    res.json({ success: true, message: "Favourite movies updated." });
+    res.json({
+      success: true,
+      message: favourites.includes(movieId)
+        ? "Removed from favourites"
+        : "Added to favourites"
+    })
+
   } catch (error) {
-    console.error(error.message);
-    res.json({ success: false, message: error.message });
+    console.error(error)
+    res.json({ success: false, message: "Server error" })
   }
-};
-
+}
 
 export const getFavourites = async (req, res) => {
-    try {
-        const user = await clerkClient.users.getUser(req.auth().userId)
-        const favourites = user.privateMetadata.favourites;
+  try {
+    const user = await clerkClient.users.getUser(req.auth().userId)
 
-        // Get movies from database
-        const movies = await Movie.find({_id: {$in: favourites}})
+    const favourites = Array.isArray(user.privateMetadata.favourites)
+      ? user.privateMetadata.favourites
+      : []
 
-        res.json({success: true, movies})
-    } catch (error) {
-        console.error(error.message);
-        res.json({ success: false, message: error.message })
-    }
+    // Get movies from database
+    const movies = await Movie.find({_id: { $in: favourites }})
+    res.json({ success: true, movies })
+  } catch (error) {
+    console.error(error)
+    res.json({ success: false, message: error.message })
+  }
 }
