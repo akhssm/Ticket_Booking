5f8da0d2010c6688cacd532d917e9fa0482a27b9
Akshay <mandaakshay20@gmail.com>
2026-02-04 12:38:29 +0530

    Made changes

--- server/server.js
+++ server/server.js
@@ -2,19 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
 import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import { stripeWebhooks } from './controllers/stripeWebhooks.js';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 const port = 3000;
 
-await connectDB()
+await connectDB();
 
-// Stripe Webhook Route
+// Stripe Webhook Route (must be raw)
 app.post("/api/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
 // Middleware
@@ -22,11 +24,15 @@ app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 
+app.use("/api/inngest", serve({client: inngest, functions,}));
+
 // API Routes
 app.get('/', (req, res) => res.send('server is Live!'));
-app.use('/api/show', showRouter)
-app.use('/api/booking', bookingRouter)
-app.use('/api/admin', adminRouter)
-app.use('/api/user', userRouter)
+app.use('/api/show', showRouter);
+app.use('/api/booking', bookingRouter);
+app.use('/api/admin', adminRouter);
+app.use('/api/user', userRouter);
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server listening at http://localhost:${port}`)
+);
